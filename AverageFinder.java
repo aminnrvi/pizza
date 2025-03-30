8190f90108182315329b5eda256d6f285db260d4
aminnrvi <ghourcn@gmail.com>
2025-03-30 20:11:55 +0500

    problem 1

---
+++ AverageFinder.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class AverageFinder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+        double average = findAverage(array);
+        System.out.println(average);
+    }
+
+    public static double findAverage(int[] array) {
+        double sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum / array.length;
+    }
+}

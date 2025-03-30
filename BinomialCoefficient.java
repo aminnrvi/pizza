6bdf27f3b5d676c1ee28fd69cf6dcf99fbdc3e02
aminnrvi <ghourcn@gmail.com>
2025-03-30 20:48:34 +0500

    problem 9

---
+++ BinomialCoefficient.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BinomialCoefficient {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = scanner.nextInt();
+        System.out.print("Enter k: ");
+        int k = scanner.nextInt();
+        System.out.println(binomialCoeff(n, k));
+        scanner.close();
+    }
+
+    public static int binomialCoeff(int n, int k) {
+        if (k == 0 || k == n) return 1;
+        return binomialCoeff(n - 1, k - 1) + binomialCoeff(n - 1, k);
+    }
+
+}
